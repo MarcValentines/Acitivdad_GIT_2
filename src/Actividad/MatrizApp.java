118a72699b545c84bde6c5ab4cde6a0e2743d57a
Carlos <131690161+Yosoyina@users.noreply.github.com>
2024-12-16 19:37:00 +0100

    Apartado donde muestra la matriz con numeros aleatorios
    En este apartado he hecho un recorrido donde se recorre por filas i columnas i en el primero antes de que se printea se hace primero los numero aleatorios
    
    y despues se printea los numero aleatorios

--- src/Actividad/MatrizApp.java
+++ src/Actividad/MatrizApp.java
@@ -1,10 +1,31 @@
 package Actividad;
-
+import java.util.Random;
 public class MatrizApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Apartado de la Matriz-Inicial ( Carlos )
+		
+		Random numAL = new Random ();
+		
+		int [][] matriz_inicial = new int [5][5];
+		
+		System.out.println("Esta es la matriz inicial con los Numeros Aleatorios: ");
+		
+		System.out.println();
+		
+		for (int i = 0; i < matriz_inicial.length; i++) {
+			for (int j = 0; j < matriz_inicial[i].length; j++) {
+				
+				matriz_inicial[i][j] = numAL.nextInt(100) +1;
+				
+				System.out.print(matriz_inicial[i][j] + "\t");
+				
+				
+			}
+			System.out.println();
+		}
+			
 	}
-
 }
